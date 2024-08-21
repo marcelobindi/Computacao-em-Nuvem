577d92ea4a11be302439c1aa7c208eee62fb4d65
marcelobindi <marcelobindi@gmail.com>
2024-08-21 14:12:24 -0300

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+def start_recognition():
+    # Código para iniciar o reconhecimento facial
+    pass
+
+root = tk.Tk()
+tk.Button(root, text="Iniciar Reconhecimento", command=start_recognition).pack()
+root.mainloop()
